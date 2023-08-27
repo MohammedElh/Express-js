4143933f458b399ef1d58a6de1127d8c1db852fa
MohammedElh <Habachi20000@gmail.com>
2023-08-27 17:17:16 +0100

    :/

--- Express-day2/app.js
+++ Express-day2/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 let products = [
     { id: 1, name: 'iPhone 12 Pro', price: 1099.99 },
     { id: 2, name: 'Samsung Galaxy S21', price: 999.99 },
@@ -8,84 +11,36 @@ let products = [
     { id: 4, name: 'MacBook Pro 16', price: 2399.99 },
     { id: 5, name: 'DJI Mavic Air 2', price: 799.99 },
 ];
-app.post('/products', (req, res) => {
-    const newProduct = req.body;
-    // Add validation and data sanitization as needed
-    newProduct.id = products.length + 1; // Assign a new ID (in a real app, this might come from a database sequence)
-  
-    // Add the new product to the products array (for demonstration purposes)
-    products.push(newProduct);
-  
-    // Respond with the newly created product and a "Created" status code (201)
-    res.status(201).json(newProduct);
-  });
-app.delete('/product/:id',(req,res)=>{
-    let selected = req.params.id;
-    for (let i = 0; i < products.length; i++) {
-        if(products[i].id===selected){
-            products.splice(1,i);
-            res.json({ message: 'Product deleted successfully' })
-        }else{
-            res.status(404).json({ message: 'Product not found' })
-        }
-    }
-  })
-// GET request to search for products based on query parameters
-app.get('/products', (req, res) => {
-    //     for (i = 0; i < products.length; i++) {
-    //         var Id=[''];
-    //         var Name=[''];
-    //         var Price=[''];
-    //         var data=[''];
-    //         Id[i] = products[i].id;
-    //         Name[i] = products[i].name;
-    //         Price[i] = products[i].price;
-    //         data[i] =`Id: ${Id[i]}, Name: ${Name[i]}, Price: ${Price[i]}`
-    // }
-    let responseHTML='';
-    products.forEach(product => {
-        responseHTML += `Product: ${product.name}, Price: ${product.price} <br>`;
-      });
-    
-      // Send the HTML response
-      res.send(responseHTML);
-});
-app.get('/products/search', (req, res) => {
-    const searchQuery = req.query.q;
-    const minPrice = req.query.minPrice;
-    const maxPrice = req.query.maxPrice;
-    // Use the searchQuery, minPrice, and maxPrice to perform a search in the database
-var array=[]
-for (let i = 0; i < products.length; i++) {
-    if (minPrice <= products[i].price && maxPrice >= products[i].price){
-    array.push(products[i]);}
-}
-let responseHTML = `Searching for products with query: ${searchQuery},
-                       minPrice: ${minPrice}, maxPrice: ${maxPrice} <br><br>`;
-  
-  // Add the search results to the HTML response
-  array.forEach(product => {
-    responseHTML += `Product: ${product.name}, Price: ${product.price} <br>`;
+
+app.use((req, res, next) => {
+    const currentDateTime = new Date().toLocaleString();
+    const requestMethod = req.method;
+    const requestUrl = req.url;
+    console.log(`--------New Request--------`);
+    console.log(`Current time: ${currentDateTime}`);
+    console.log(`request method: ${requestMethod}`);
+    console.log(`request URL: ${requestUrl}`);
+    next();
   });
 
-  // Send the HTML response
-  res.send(responseHTML);
-//http://localhost:3000/products/search?q=shoes&minPrice=20&maxPrice=100
-});
-// GET request to retrieve a specific product by ID
-app.get('/products/:id', (req, res) => {
-    let selected = req.params.id;
+const productsRouter = require('./routes/products');
+app.use('/products', productsRouter);
 
-    // Use the productId to fetch the corresponding product from the database
-    // for (i = 0; i < products.length; i++) {
-    //     if (productId == products[i].id) {
-    //         selected = products[i];
-    //     }
-    // }
-    // Return the product information as the response
-    res.send(`Product ID : ${(products[selected - 1].id)}. <br> Product Name : ${(products[selected - 1].name)}. <br> Product Price : ${(products[selected - 1].price)}`);
-});
-
+app.all('*',(req,res,next)=>{
+    const err = new Error(`Can't find "${req.originalUrl}" on the server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    console.log(`Error: ${err.message}`);
+    next(err)
+})
+app.use((error,req,res,next)=>{
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'error';
+    res.status(error.statusCode).json({
+        status : error.statusCode,
+        message : error.message
+    })
+})
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
---
+++ Express-day2/routes/products.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router()
+
+
+let products = [
+    { id: 1, name: 'iPhone 12 Pro', price: 1099.99 },
+    { id: 2, name: 'Samsung Galaxy S21', price: 999.99 },
+    { id: 3, name: 'Sony PlayStation 5', price: 499.99 },
+    { id: 4, name: 'MacBook Pro 16', price: 2399.99 },
+    { id: 5, name: 'DJI Mavic Air 2', price: 799.99 },
+    { id: 6, name: 'iPhone 12 Pro', price: 1099.99 },
+    { id: 7, name: 'Samsung Galaxy S21', price: 999.99 },
+    { id: 8, name: 'Sony PlayStation 5', price: 499.99 },
+    { id: 9, name: 'MacBook Pro 16', price: 2399.99 },
+    { id: 10, name: 'DJI Mavic Air 2', price: 799.99 },
+];
+
+router.get('/', (req, res) => {
+    let responseHTML = '';
+    products.forEach(product => {
+        responseHTML += `Product: ${product.name}, Price: ${product.price} <br>`;
+    });
+
+    // Send the HTML response
+    res.send(responseHTML);
+});
+router.get('/search', (req, res) => {
+    const searchQuery = req.query.q;
+    const minPrice = req.query.minPrice;
+    const maxPrice = req.query.maxPrice;
+    // Use the searchQuery, minPrice, and maxPrice to perform a search in the database
+    var array = []
+    for (let i = 0; i < products.length; i++) {
+        if (minPrice <= products[i].price && maxPrice >= products[i].price) {
+            array.push(products[i]);
+        }
+    }
+    let responseHTML = `Searching for products with query: ${searchQuery},
+                       minPrice: ${minPrice}, maxPrice: ${maxPrice} <br><br>`;
+
+    // Add the search results to the HTML response
+    array.forEach(product => {
+        responseHTML += `Product: ${product.name}, Price: ${product.price} <br>`;
+    });
+
+    // Send the HTML response
+    res.send(responseHTML);
+    //http://localhost:3000/products/search?q=shoes&minPrice=20&maxPrice=100
+});
+router.get('/:id', (req, res) => {
+    let selected = req.params.id;
+    if (products[selected]) {
+        res.send(`Product ID : ${(products[selected - 1].id)}. <br> Product Name : ${(products[selected - 1].name)}. <br> Product Price : ${(products[selected - 1].price)}`);
+    } else {
+        res.status(404).send('Product not found')
+    }
+});
+router.post('/', (req, res) => {
+    if (req.body.id) {
+        const newProduct = req.body;
+        products.push(newProduct);
+        console.log(products);
+        res.status(201).json(newProduct);
+    } else {
+        const err = new Error('Invalid product format.');
+        err.status = 404;
+        res.status(err.status).json({ message: err.message });
+    }
+});
+router.delete('/:id', (req, res) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id == req.params.id) {
+            var selectedProduct = products[i];
+            products.splice(i, 1);
+        }
+    }
+    console.log(products);
+    res.status(200).json(selectedProduct || "Product not found");
+}
+);
+router.put('/:id', (req, res) => {
+    LastId = products[products.length - 1].id;
+
+    let selected = req.params.id;
+    if (products[selected]) {
+        products.splice(1, i, userInput);
+        res.json({ message: 'Product updated successfully' })
+    } else {
+        res.status(404).json({ message: 'Product not found' })
+    }
+})
+
+module.exports = router;
